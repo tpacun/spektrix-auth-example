b37a0a3034d93187e6c99eeec9016aba94253450
tpacun <62911461+tpacun@users.noreply.github.com>
2022-11-11 17:14:40 -0500

    auth introduced into interface, need to solve issue with timezones and clocks out of sync for system to accept request

--- api.js
+++ api.js
@@ -1,26 +1,31 @@
-import { getRequest } from './interface.js'
+import SpektrixRequest from './interface.js'
 import { getAuthorization } from './authorization.js'
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 
 class Spektrix {
 
-    constructor(domain, clientName, username, secretKey) {
-        this.connectionConfig = {
+    constructor({domain, clientName, username, secretKey}) {
+        this.spektrixRequest = new SpektrixRequest({
             domain: domain, 
             clientName: clientName,
             username: username,
-            secretKey: secretKey}
+            secretKey: secretKey
+        })
     }
 
     async events() {
-        let events = await getRequest('events')
+        let events = await this.spektrixRequest.getRequest('customers?email=theo.pacun@spektrix.com', true)
         return events
     }
 
 
 }
 
-let apiTesting = new Spektrix
+let apiTesting = new Spektrix({domain: 'system.spektrix.com', clientName: 'apitesting', username: process.env.USERNAME, secretKey: process.env.SECRETKEY})
 let apiRes = await apiTesting.events()
 console.log(apiRes[0])
+
+
 
--- interface.js
+++ interface.js
@@ -1,12 +1,24 @@
 import axios from 'axios';
+import { getAuthorization } from './authorization.js';
 
-async function getRequest(endpoint, config = {domain: 'system.spektrix.com', clientName: 'apitesting', username: '', secretKey: ''}) {
-    try {
-        const res = await axios.get(`https://${config.domain}/${config.clientName}/api/v3/${endpoint}`)
-        return res.data
-    } catch (error) {
-        console.log(error)
+export default class SpektrixRequest {
+    constructor(config = {domain: 'system.spektrix.com', clientName: 'apitesting', username: '', secretKey: ''}) {
+        this.domain = config.domain
+        this.clientName = config.clientName
+        this.username = config.username
+        this.secretKey = config.secretKey
+        this.clientUrl = `https://${this.domain}/${this.clientName}/api/v3/`
+    }
+
+    async getRequest(endpoint, auth = false) {
+        const datetime = new Date(Date.now()).toUTCString()
+        console.log(datetime)
+        let authHeader = auth ? getAuthorization('GET', this.username, datetime, this.secretKey, this.clientUrl + endpoint, null):null
+        try {
+            const res = await axios.get(`https://${this.domain}/${this.clientName}/api/v3/${endpoint}`, {headers: {'Authorization': authHeader, 'Date': datetime, 'Host': this.domain}})
+            return res.data
+        } catch (error) {
+            console.log(error)
+        }
     }
 }
-
-export { getRequest }
